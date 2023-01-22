078e695e2939f82d8a32ddf73fd2e8dd67bb5a06
asiajanicka <asia.janicka@gmil.com>
2023-01-23 00:34:02 +0100

    feat: DOM parser test: schema validation & simple parsed data tests

---
+++ src/test/java/org/example/parserTest/DOMTests.java
@@ -0,0 +1,46 @@
+package org.example.parserTest;
+
+import org.example.model.StudentGroup;
+import org.example.utils.XmlUtils;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.validation.Schema;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DOMTests {
+
+    @Test
+    public void validateXMLAgainstSchema() throws ParserConfigurationException, IOException, SAXException {
+
+        Schema schema = XmlUtils.getSchema("src/test/resources/studentGroups.xsd");
+        Document document = XmlUtils.parseXml("src/test/resources/studentGroups.xml");
+        assertThat(XmlUtils.validateXml(schema, document)).isTrue();
+
+        List<StudentGroup> studentGroupsFromXML = XmlUtils.getStudentGroupsFromDocument(document);
+
+        assertThat(studentGroupsFromXML.size()).isEqualTo(2);
+
+        assertThat(studentGroupsFromXML.get(0).getStudents().size()).isEqualTo(2);
+        assertThat(studentGroupsFromXML.get(0).getStudents()
+                .stream()
+                .filter(p -> (p.getPortalAccount() != null))
+                .count())
+                .isEqualTo(2);
+        assertThat(studentGroupsFromXML.get(0).getStudents().get(0).getGrades().size()).isEqualTo(3);
+
+        assertThat(studentGroupsFromXML.get(1).getStudents().size()).isEqualTo(3);
+        assertThat(studentGroupsFromXML.get(1).getStudents()
+                .stream()
+                .filter(p -> (p.getPortalAccount() != null))
+                .count())
+                .isEqualTo(2);
+        assertThat(studentGroupsFromXML.get(1).getStudents().get(2).getGrades()).isEmpty();
+    }
+
+}
