9505bce78ecf43b39924cf502f6651c8ae0c675b
asiajanicka <asia.janicka@gmil.com>
2023-01-26 10:54:46 +0100

    style: removed unused import in JaxbTest & change of date format in LocalDateAdapter

--- src/main/java/org/example/utils/LocalDateAdapter.java
+++ src/main/java/org/example/utils/LocalDateAdapter.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 @NoArgsConstructor
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public LocalDate unmarshal(String date) {
--- src/test/java/org/example/parserTest/JaxbTests.java
+++ src/test/java/org/example/parserTest/JaxbTests.java
@@ -12,7 +12,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
