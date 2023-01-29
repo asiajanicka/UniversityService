73f5e5bc92e683e82ec7ebeceed3124069f65098
asiajanicka <asia.janicka@gmil.com>
2023-01-29 15:24:06 +0100

    feat: added mappers and new services implementation test: added tests for BuildingServiceImpl, DepartmentServiceImpl, ParkingSpotServiceImpl, SubjectServiceImpl
    ServiceImpls for Subject, Building + Room, Department + Teacher, Parking Spot

--- src/main/java/org/example/dao/interfaces/IRoomDAO.java
+++ src/main/java/org/example/dao/interfaces/IRoomDAO.java
@@ -1,15 +1,16 @@
 package org.example.dao.interfaces;
 
+import org.apache.ibatis.annotations.Param;
 import org.example.model.Room;
 
 import java.util.List;
 
 public interface IRoomDAO extends IBaseDAO<Room> {
 
-    int bindRoomToBuildingId(long roomId, long buildingId);
+    int bindRoomToBuildingId(@Param("roomId") long roomId, @Param("buildingId") long buildingId);
 
-    List<Room> getRoomsByBuildingId(long teacherId);
+    List<Room> getRoomsByBuildingId(@Param("teacherId")long teacherId);
 
-    int removeRoomsByBuildingId(long buildingId);
+    int removeRoomsByBuildingId(@Param("buildingId") long buildingId);
 
 }
