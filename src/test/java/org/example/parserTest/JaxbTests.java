b71e50871bc51626100ec9f27eeeff8a53bc6354
asiajanicka <asia.janicka@gmil.com>
2023-02-03 14:32:00 +0100

    refactor: add timestamp to xml created in tests

--- src/test/java/org/example/parserTest/JaxbTests.java
+++ src/test/java/org/example/parserTest/JaxbTests.java
@@ -6,6 +6,7 @@ import org.example.model.StudentGroup;
 import org.example.service.StudentService;
 import org.example.service.exception.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
+import utils.TestUtils;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -22,19 +23,19 @@ public class JaxbTests {
     @Test
     public void usecaseTest() throws EntityNotFoundException, JAXBException {
 
-        String path = "src/test/java/org/example/testResults/studentGroupsJAXB.xml";
+        File file = new File (TestUtils.getNameForXMLFile("studentGroupJAXB"));
         StudentService studentService = new StudentService();
         StudentGroup groupFromDB = studentService.getStudentGroupById(1);
         logger.info("Retrieved student group from DB");
         JAXBContext context = JAXBContext.newInstance(StudentGroup.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(groupFromDB, new File(path));
-        logger.info(String.format("Marshalled student group to file '%s'", path));
+        marshaller.marshal(groupFromDB, file);
+        logger.info(String.format("Marshalled student group to file '%s'", file));
 
         Unmarshaller unmarshaller = context.createUnmarshaller();
-        StudentGroup groupFromXML = (StudentGroup) unmarshaller.unmarshal(new File(path));
-        logger.info(String.format("Unmarshalled student group from file '%s' to object", path));
+        StudentGroup groupFromXML = (StudentGroup) unmarshaller.unmarshal(file);
+        logger.info(String.format("Unmarshalled student group from file '%s' to object", file));
         assertThat(groupFromXML).isEqualTo(groupFromDB);
     }
 
